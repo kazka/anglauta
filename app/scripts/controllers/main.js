0f0ecc882eeef2cd56145ff08dbca3adcba9284f
kaka <khacir@gmail.com>
2014-10-19 18:05:20 +0300

    kuvan lähetys urlilla

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,30 +8,36 @@
  * Controller of the anglautaApp
  */
 
-var app = angular.module('anglautaApp')
+var app = angular.module('anglautaApp', []);
 
-app.controller('MainCtrl', function ($scope, $http) {
+app.controller('MainCtrl', function ($scope, $http, $filter) {
     $scope.ketju = {};
     $scope.msg = {};
 
     $http.get('http://anglauta.herokuapp.com/ketjus.json').success( function(data, status, headers, config) {
-        console.log(data)
+        console.log(data);
         $scope.ketjus = data;
     });
 
     $scope.createKetju = function() {
         $http.post('http://anglauta.herokuapp.com/ketjus.json', $scope.ketju).success( function(data, status, headers, config) {
-            $scope.ketjus.push(data)
+            $scope.ketjus.push(data);
+            $scope.createMsg(data.id);
         });
-        $scope.ketju = {}
-        $scope.createMsg();
+        $scope.ketju = {};
     };
 
     $scope.createMsg = function($ketjuid) {
+        console.log($scope.msg);
         $scope.msg.ketju_id = $ketjuid;
         $http.post('http://anglauta.herokuapp.com/messages.json', $scope.msg).success( function(data, status, headers, config) {
-            $scope.messages.push(data)
+            var ketju = $filter('filter')($scope.ketjus, {id: $ketjuid}, true);
+            if (ketju.length) {
+                ketju[0].messages.push(data);
+            }
         });
-        $scope.msg = {}
+        $scope.msg = {};
     }
 });
+
+
